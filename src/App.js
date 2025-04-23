488e0fc1fb37f76d8607c96ecd279aaeaa1bcb10
Brian <briankagia24@gmail.com>
2025-04-23 07:31:29 +0300

    login/sign in form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 
-import React from "react";
+// import React from "react";
+import React, { useState } from 'react';
+import LoginForm from "./Components/LoginForm"
 import WeatherDashboard from "./Components/WeatherDashboard";
 
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
+
+
   return (
     <div className="App">
-      <WeatherDashboard />
+      {!showLogin ? (
+        <>
+        <button onClick={() => setShowLogin(true)} className="btn btn-success mt-3">
+            Login
+          </button>
+          <WeatherDashboard />
+          
+        </>
+      ) : (
+        <LoginForm />
+      )}
+      
     </div>
   );
 }
