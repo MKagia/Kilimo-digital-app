4dd0d43cd4300b8472fb44e2cef69b4454ac58c2
Brian <briankagia24@gmail.com>
2025-04-29 06:42:51 +0300

    Adds onClick and onChange Cart features

--- src/Components/Cart/CartItem.jsx
+++ src/Components/Cart/CartItem.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from '../Context/ShopContext';
+
 
 const CartItem = (props) => {
-    const { tool_name, price_per_unit } = props.data;
+    const { tool_id, tool_name, price_per_unit, toolimage } = props.data;
+    const { cartItems, removeFromCart, addToCart, updateCartItemCount } = useContext(ShopContext);
 
 
   return (
     <div className='cartItem'>
 
-        <img src='toolimage' alt=''/>
+        <img src={toolimage} alt=''/>
         <div className='description'>
             <p> <b> {tool_name} </b></p>
             <p> Ksh {price_per_unit}</p>
+            <div className='countHandler'>
+                <button onClick={() => removeFromCart(tool_id)}> - </button>
+                <input value={cartItems[tool_id]} onChange={(e) => updateCartItemCount(Number(e.target.value), tool_id)}/>
+
+                <button onClick={() => addToCart(tool_id)}> + </button>
+
+            </div>
 
         </div>
     </div>
